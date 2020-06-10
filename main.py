80f0dc06adc2dc90c50bad15c9a8e6fae26a6903
Martin Jurkovic <mj5835@student.uni-lj.si>
2020-06-10 13:14:11 +0200

    Added flags for velika or casopisna citalnica

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from selenium import webdriver
 from time import sleep
 from secrets import pw, username
@@ -25,20 +26,40 @@ class NukReservation:
     sleep(4)
     print("LOGGED IN!\n")
     
-    
+    self.location = sys.argv[1]
+    if (self.location == "vc" or not self.location):
+      print("TRYING FOR VELIKA ČITALNICA!\n")
+      # SELECT ALL
+      self.driver.find_element_by_xpath('//label[@for="selectAllSwitch1"]').click()
+      print("Selected all!\n")
+      sleep(0.5)
 
-    # SELECT ALL
-    self.driver.find_element_by_xpath('//label[@for="selectAllSwitch1"]').click()
-    print("Selected all!\n")
-    sleep(0.5)
+      self.driver.execute_script("window.scrollTo(0, 50)") 
+      print("SCROLLED\n")
+      sleep(1)
 
-    self.driver.execute_script("window.scrollTo(0, 50)") 
-    print("SCROLLED\n")
-    sleep(1)
+      self.driver.find_element_by_xpath("//button[contains(text(), 'Rezerviraj')]").click()
+      print("REZERVIRANO!")
+      sleep(0.5)
+    elif (self.location == "cc"):
+      print("TRYING FOR ČASOPISNA ČITALNICA!\n")
+      # scroll into view
+      element = self.driver.find_element_by_xpath("//strong[contains(text(), 'Časopisna čitalnica')]")
+      self.driver.execute_script("arguments[0].scrollIntoView();", element) 
+      print("SCROLLED\n")
+      sleep(1)
 
-    self.driver.find_element_by_xpath("//button[contains(text(), 'Rezerviraj')]").click()
-    print("REZERVIRANO!")
-    sleep(0.5)
+      # SELECT ALL
+      self.driver.find_element_by_xpath('//label[@for="selectAllSwitch2"]').click()
+      print("Selected all!\n")
+      sleep(0.5)
+
+      self.driver.find_element_by_xpath("/html/body/main/div/div[2]/div[3]/div[2]/div/div[3]/button").click()
+      print("REZERVIRANO!")
+      sleep(0.5)
+    else:
+      print("Wrong argument!")
+
 
     print("END\n") 
     sleep(4)
